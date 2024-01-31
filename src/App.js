5741b07f0ca423b84af0d0212e30aa76a0188813
Jasmin Hidanovic <dexinho008@gmail.com>
2024-01-31 23:47:05 +0100

    added ListWantedUser component

--- src/App.js
+++ src/App.js
@@ -1,36 +1,9 @@
-import React, { useEffect, useState } from "react";
 import { ListWantedUsers } from "./components/ListWantedUsers/ListWantedUsers";
 
 const App = () => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [wantedList, setWantedList] = useState([]);
-  const [currentPage, setCurrentPage] = useState(1);
-
-  useEffect(() => {
-    const handleFetchData = async () => {
-      try {
-        setIsLoading(true);
-        const response = await fetch(
-          `https://api.fbi.gov/wanted/v1/list?page=${currentPage}`
-        );
-
-        const data = await response.json();
-        setWantedList([...data.items])
-      } catch (err) {
-        console.log(err);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-
-    handleFetchData();
-  }, [currentPage]);
-
-  if (isLoading) return <div>Loading...</div>;
-
   return (
     <div>
-      <ListWantedUsers wantedList={wantedList}/>
+      <ListWantedUsers />
     </div>
   );
 };
