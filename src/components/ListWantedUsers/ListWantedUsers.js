0a0cf4022f154dda543a37221ae64d7065d13ee2
Jasmin Hidanovic <dexinho008@gmail.com>
2024-01-31 23:47:18 +0100

    added list useState

--- src/components/ListWantedUsers/ListWantedUsers.js
+++ src/components/ListWantedUsers/ListWantedUsers.js
@@ -1,18 +1,64 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { Pagination } from "../Pagination/Pagination";
 
-export const ListWantedUsers = ({ wantedList }) => {
-  console.log(wantedList);
+export const ListWantedUsers = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [wantedList, setWantedList] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalNumberOfPages, setTotalNumberOfPages] = useState(1);
+
+  useEffect(() => {
+    const handleFetchData = async () => {
+      try {
+        setIsLoading(true);
+        console.log(currentPage);
+        const response = await fetch(
+          `https://api.fbi.gov/wanted/v1/list?page=${currentPage}`
+        );
+
+        const data = await response.json();
+
+        setTotalNumberOfPages(Math.ceil(data.total / data.items.length));
+        setWantedList([...data.items]);
+      } catch (err) {
+        console.log(err);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    handleFetchData();
+  }, [currentPage]);
+
+  if (isLoading) return <div>Loading...</div>;
+
+  console.log(wantedList[0])
+
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(Number(pageNumber));
+  };
+
   return (
-    <div>
-      <ol>
+    <div style={{ padding: "1rem" }}>
+      <div style={{ padding: "1rem" }}>
         {wantedList.map((user, index) => (
-          <li key={index}>
+          <div key={index} style={{ border: "2px solid", padding: "1rem" }}>
             <div>{user.title}</div>
-            <img src={user.images[0].large} />
-            <div>{user.caution.replace(//g, '')}</div>
-          </li>
+            <img
+              src={user.images[0].large}
+              style={{ width: "100px" }}
+              alt={user.title}
+            />
+            <div>{user.description ? user.description : 'No description'}</div>
+          </div>
         ))}
-      </ol>
+      </div>
+      <Pagination
+        currentPage={currentPage}
+        numberOfPageSlots={5}
+        totalNumberOfPages={totalNumberOfPages}
+        onPageChange={handlePageChange}
+      />
     </div>
   );
 };
